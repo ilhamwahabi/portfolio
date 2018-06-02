0bb0bbf5ca2a9ded710b2407c17dbaa17b8dbe24
Ilham Wahabi <wahabiputra@gmail.com>
2018-06-03 06:31:34 +0700

    Add Header img

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,15 +5,20 @@ import './Header.css'
 export default class Header extends React.Component {
   render(){
     return (
-      <header className="header">
-        <div className="header__content">
-          <div className="header__main">
-            <h1 className="header__title">Amazing Things</h1>
-            <p className="header__subtitle">by <span className="header__name">Ilham Wahabi</span></p>
+      <React.Fragment>
+        <header className="header">
+          <div className="header__content">
+            <div className="header__main">
+              <h1 className="header__title">Amazing Things</h1>
+              <p className="header__subtitle">by <span className="header__name">Ilham Wahabi</span></p>
+            </div>
           </div>
-        </div>
-        <div className="header__divider"></div>        
-      </header>
-    )
+          <div className="header__divider">
+            <div className="header__img"></div>
+          </div>
+        </header>
+        </React.Fragment>
+      )
+    }
   }
-}
+  // <div className="mockup"></div>
