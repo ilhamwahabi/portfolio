aaf6913ec781066b233e0fa5c965e1dcc7fa5247
Ilham Wahabi <wahabiputra@gmail.com>
2018-06-03 14:55:45 +0700

    Add auto-hide Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,9 +3,36 @@ import React from 'react'
 import './Navbar.css'
 
 export default class Navbar extends React.Component {
+  state = {
+    scrollClass: '',
+    scrolledHeight: 0
+  }
+
+  componentDidMount = () => {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  componentWillUnmount = () => {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    var minScroll = window.innerHeight * 1;
+  
+    if (
+      window.scrollY > this.state.scrolledHeight
+    ) {
+      this.setState({ scrollClass: 'navbar__hide' })      
+    } else {
+      this.setState({ scrollClass: '' })
+    }
+
+    this.setState({ scrolledHeight: window.scrollY})
+  }
+
   render(){
     return (
-      <nav className="navbar">
+      <nav className={`navbar ${this.state.scrollClass}`}>
         <div className="navbar__row">
           <p className="navbar__logo">Amazing Things</p>
           <div className="divider5"></div>          
