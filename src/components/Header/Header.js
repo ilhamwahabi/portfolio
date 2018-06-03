a68d7911065b7e2f9c5659d2db01b7153799e70c
Ilham Wahabi <wahabiputra@gmail.com>
2018-06-03 13:21:24 +0700

    Add arrow, change header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,14 +7,19 @@ export default class Header extends React.Component {
     return (
       <React.Fragment>
         <header className="header">
-          <div className="header__content">
-            <div className="header__main">
-              <h1 className="header__title">Amazing Things</h1>
-              <p className="header__subtitle">by <span className="header__name">Ilham Wahabi</span></p>
+          <div className="header__row">
+            <div className="header__content">
+              <div className="header__main">
+                <h1 className="header__title">Awesome Projects</h1>
+                <p className="header__subtitle">by <span className="header__name">Ilham Wahabi</span></p>
+              </div>
+            </div>
+            <div className="header__divider">
+              <div className="header__img"></div>
             </div>
           </div>
-          <div className="header__divider">
-            <div className="header__img"></div>
+          <div className="header__icon">
+            <i className="material-icons md-48">keyboard_arrow_down</i>
           </div>
         </header>
         </React.Fragment>
