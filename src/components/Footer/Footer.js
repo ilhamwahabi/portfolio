d15845b52b48d82e7d0061912743e4afb54838c5
Ilham Wahabi <wahabiputra@gmail.com>
2018-07-20 20:06:29 +0700

    Responsive footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,12 +7,12 @@ export default class Footer extends React.Component {
     return (
       <footer className="footer">
         <div className="divider1"></div>      
-        <p className="footer__copy">&copy; 2018 Ilham Wahabi. All Rights Reserved</p>
+        <p className="footer__copy">&copy; 2018 Ilham Wahabi. <br/> All Rights Reserved</p>
         <div className="divider3"></div>
         <div className="footer__link">
-          <a href="https://github.com/IlhamWahabiGX"><p>Github</p></a>
-          <a href="https://www.linkedin.com/in/ilham-wahabi-aa1479131/"><p>LinkedIn</p></a>
-          <a href="https://drive.google.com/open?id=1m0MwAYj3GLWRCuFUzzAHQqXabSB6cNul"><p>My CV</p></a>
+          <a className="footer__link-github" href="https://github.com/IlhamWahabiGX"><p>Github</p></a>
+          <a className="footer__link-linkedin" href="https://www.linkedin.com/in/ilham-wahabi-aa1479131/"><p>LinkedIn</p></a>
+          <a className="footer__link-cv" href="https://drive.google.com/open?id=1m0MwAYj3GLWRCuFUzzAHQqXabSB6cNul"><p>My CV</p></a>
         </div>
         <div className="divider1"></div>        
       </footer>
