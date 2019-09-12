96f40fd38c2e034ba49b4d2e64b0873092cf917c
Ilham Wahabi <wahabiputra@gmail.com>
2019-09-12 09:18:01 +0700

    feat: Optimization - enable prerendering

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { hydrate, render } from "react-dom";
 
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
+
 registerServiceWorker();
