ccb2515a20c744759abd12f2b882de6dd6c1f2f2
iwgx <wahabiputra@gmail.com>
2020-06-29 19:20:30 +0700

    Remove views/Home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 
-import Home from "./views/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import Desc from "./components/Desc/Desc";
+import Work from "./components/Work/Work";
+import Footer from "./components/Footer/Footer";
 
 import "./App.css";
 
 const App = () => (
   <div className="App">
-    <Home />
+    <Navbar />
+    <Header />
+    <Desc />
+    <Work />
+    <Footer />
   </div>
 );
 
