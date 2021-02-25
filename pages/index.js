28300152acbedaccd5ffe30fe2cfd96f37be5af0
iwgx <wahabiputra@gmail.com>
2021-02-26 04:37:05 +0700

    Update react & react-dom to 16.14.0 and next@10.0.7

--- pages/index.js
+++ pages/index.js
@@ -116,7 +116,7 @@ export default function Home({ works }) {
   return (
     <div className="App">
       <Head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
